644fee4d32eda67c7e57e03104232f5e79f5c102
Sandoleathy <3296446003@qq.com>
2023-09-21 13:46:14 +0800

    create android project

---
+++ Android/app/src/main/java/andorid/live_on/MainActivity.java
@@ -0,0 +1,13 @@
+package andorid.live_on;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        
+    }
+}
